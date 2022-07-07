10372ae5eac77438cca3fda81285bf5b1172dde6
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2022-07-07 15:16:10 +0200

    Add progress bar when downloading challenges

--- download_challenges.py
+++ download_challenges.py
@@ -2,30 +2,29 @@ import os
 import shutil
 
 import requests
+from tqdm import tqdm
 
 """ Download all challenges from the website """
 
-NUM_CHALLENGES = 346
+MIN_CHALLENGE_ID = 3
+MAX_CHALLENGE_ID = 346
 
 WHIBOX_BASE_URL = "https://whibox.io/contests/2021/candidate/"
 CHALLENGES_PATH = "challenges"
 
-if os.path.exists(CHALLENGES_PATH):
-    shutil.rmtree(CHALLENGES_PATH)
-os.mkdir(CHALLENGES_PATH)
-
 
 def main():
+    if os.path.exists(CHALLENGES_PATH):
+        shutil.rmtree(CHALLENGES_PATH)
+    os.mkdir(CHALLENGES_PATH)
+
     session = requests.Session()
-    for i in range(3, NUM_CHALLENGES + 1):
-        print(i)
+    for i in tqdm(range(MIN_CHALLENGE_ID, MAX_CHALLENGE_ID + 1)):
         challenge_base_url = f"{WHIBOX_BASE_URL}/{i:d}/"
         challenge_base_dir = os.path.join(CHALLENGES_PATH, str(i))
 
         for filename in ("source.c", "pubkey"):
             req = session.get(challenge_base_url + filename, allow_redirects=False)
-            print(challenge_base_url + filename)
-            print(i, req.status_code)
             if req.status_code != 200:
                 continue
 
